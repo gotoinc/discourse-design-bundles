1c6e4a81cf02bd6f445afb2fcdefd0a054aba8a1
AntonTkachov <lookformeon@gmail.com>
2020-04-17 15:59:08 +0300

    Extend post object with external_user_id value

--- plugin.rb
+++ plugin.rb
@@ -2,3 +2,7 @@
 # about: Discourse plugin to implement all extensions needed for Design Bundles company
 # version: 0.0.1
 # authors: Gotoinc (gotoinc.co)
+
+add_to_serializer(:post, :external_user_id, false) do
+  object&.user&.single_sign_on_record&.external_id
+end
