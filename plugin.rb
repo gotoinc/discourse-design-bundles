e640ac9e17eb6334d60dd62d0f29793aecc22632
AntonTkachov <lookformeon@gmail.com>
2020-04-24 17:45:13 +0300

    Cache active shop ids for advertisement

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 
 # name: DiscourseDesignBundles
 # about: Discourse plugin to implement all extensions needed for Design Bundles company
-# version: 0.1
+# version: 0.2
 # authors: Gotoinc
 # url: https://github.com/gotoinc/discourse-design-bundles/
 
@@ -12,4 +12,21 @@ after_initialize do
   add_to_serializer(:post, :external_user_id, false) do
     object&.user&.single_sign_on_record&.external_id
   end
+
+  class ::Jobs::CacheStoreIdsForAdvertisement < ::Jobs::Scheduled
+    every 10.minutes
+
+    def execute(args)
+      store_ids = Post.joins("INNER JOIN user_custom_fields
+                  ON user_custom_fields.user_id = posts.user_id")
+          .where("posts.created_at > ?
+                  AND user_custom_fields.name = 'storeId'
+                  AND user_custom_fields.user_id > 0
+                  AND user_custom_fields.value IS NOT NULL", 1.week.ago)
+          .pluck('user_custom_fields.value').uniq
+      Discourse.cache.write(:store_ids_for_advertisement, store_ids)
+    end
+
+  end
+
 end
