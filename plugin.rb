f736b8915f56f42d9c27a4afe441c87363f4d77c
AntonTkachov <lookformeon@gmail.com>
2020-05-05 13:59:47 +0300

    Bump version to 0.4

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 
 # name: DiscourseDesignBundles
 # about: Discourse plugin to implement all extensions needed for Design Bundles company
-# version: 0.3
+# version: 0.4
 # authors: Gotoinc
 # url: https://github.com/gotoinc/discourse-design-bundles/
 
