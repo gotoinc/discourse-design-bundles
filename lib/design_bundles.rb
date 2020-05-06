6080de1d7999f1ec0dc328f56a625d01e20cfb98
Gotoinc <root@gotoinc.co>
2020-05-06 17:24:09 +0300

    Fixed hash param

--- lib/design_bundles.rb
+++ lib/design_bundles.rb
@@ -7,9 +7,9 @@ module DesignBundles
   def self.product_display_html(store_id, user_id)
     return '' if SiteSetting.DiscourseDesignBundles_api_url.strip.empty?
 
-    store_hash = Digest::SHA1.hexdigest(SiteSetting.DiscourseDesignBundles_hash_key + store_id.to_s)
+    hash = Digest::SHA1.hexdigest(SiteSetting.DiscourseDesignBundles_hash_key + store_id.to_s)
 
-    uri = URI.parse("#{SiteSetting.DiscourseDesignBundles_api_url}?store=#{store_hash}&user=#{user_id}")
+    uri = URI.parse("#{SiteSetting.DiscourseDesignBundles_api_url}?store=#{store_id}&user=#{user_id}&hash=#{hash}")
 
     response = Net::HTTP.get_response(uri)
 
