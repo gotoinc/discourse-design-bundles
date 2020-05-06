03853456e52a941e4e014c8e9346b42587583de8
Gotoinc <root@gotoinc.co>
2020-05-06 17:13:11 +0300

    Fix return statement

--- lib/design_bundles.rb
+++ lib/design_bundles.rb
@@ -5,7 +5,7 @@ require 'net/http'
 
 module DesignBundles
   def self.product_display_html(store_id, user_id)
-    return if SiteSetting.DiscourseDesignBundles_api_url.strip.empty?
+    return '' if SiteSetting.DiscourseDesignBundles_api_url.strip.empty?
 
     store_hash = Digest::SHA1.hexdigest(SiteSetting.DiscourseDesignBundles_hash_key + store_id.to_s)
 
@@ -13,6 +13,10 @@ module DesignBundles
 
     response = Net::HTTP.get_response(uri)
 
-    response.body if response.is_a?(Net::HTTPSuccess)
+    if response.is_a?(Net::HTTPSuccess)
+      return response.body
+    else
+      return ''
+    end
   end
 end
