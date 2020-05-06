bc9da0892fef0aae06f4e5d42af3c4cf59eccbdd
Gotoinc <root@gotoinc.co>
2020-05-06 15:47:16 +0300

    Fix http errors catching

--- lib/design_bundles.rb
+++ lib/design_bundles.rb
@@ -6,13 +6,13 @@ require 'net/http'
 module DesignBundles
   def self.product_display_html(store_id, user_id)
     return if SiteSetting.DiscourseDesignBundles_api_url.strip.empty?
-    
+
     store_hash = Digest::SHA1.hexdigest(SiteSetting.DiscourseDesignBundles_hash_key + store_id.to_s)
 
     uri = URI.parse("#{SiteSetting.DiscourseDesignBundles_api_url}?store=#{store_hash}&user=#{user_id}")
 
-    code = Net::HTTP.get_response(uri).code.to_s[0]
+    response = Net::HTTP.get_response(uri)
 
-    uri.read unless code == 4 && code == 5
+    response.body if response.is_a?(Net::HTTPSuccess)
   end
 end
