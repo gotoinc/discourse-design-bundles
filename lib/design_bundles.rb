15fa03accfdfc56f5a37d52c5ea142d0a70aa8d0
Gotoinc <root@gotoinc.co>
2020-05-06 14:01:45 +0300

    Fix hashing

--- lib/design_bundles.rb
+++ lib/design_bundles.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-require 'openssl'
+require 'digest/sha1'
 require 'net/http'
 
 module DesignBundles
   def self.product_display_html(store_id, user_id)
-    store_hash = OpenSSL::HMAC.hexdigest('sha1', SiteSetting.DiscourseDesignBundles_hash_key, "#{store_id}")
+    store_hash = Digest::SHA1.hexdigest(SiteSetting.DiscourseDesignBundles_hash_key + store_id.to_s)
 
     uri = URI.parse("#{SiteSetting.DiscourseDesignBundles_api_url}?store=#{store_hash}&user=#{user_id}")
 
