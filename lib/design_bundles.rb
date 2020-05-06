67bf0767963870b7e60a068cc8b35fd36a872469
Gotoinc <root@gotoinc.co>
2020-05-06 11:33:43 +0300

    Add hashing for store_id

--- lib/design_bundles.rb
+++ lib/design_bundles.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
+require 'openssl'
+require 'net/http'
+
 module DesignBundles
   def self.product_display_html(store_id, user_id)
-    URI.parse("#{SiteSetting.DiscourseDesignBundles_api_url}?store=#{store_id}&user=#{user_id}").read
+    store_hash = OpenSSL::HMAC.hexdigest('sha1', SiteSetting.DiscourseDesignBundles_hash_key, "#{store_id}")
+
+    uri = URI.parse("#{SiteSetting.DiscourseDesignBundles_api_url}?store=#{store_hash}&user=#{user_id}")
+
+    code = Net::HTTP.get_response(uri).code.to_s[0]
+
+    uri.read unless code == 4 && code == 5
   end
 end
