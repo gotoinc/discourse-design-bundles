370f3f28084ffda430dedcec34f034df626acac2
Gotoinc <root@gotoinc.co>
2020-05-07 16:35:06 +0300

    Refactor ShopAdvertisementSerializerMixin

--- app/serializers/shop_advertisement_serializer_mixin.rb
+++ app/serializers/shop_advertisement_serializer_mixin.rb
@@ -6,24 +6,14 @@ module ShopAdvertisementSerializerMixin
   end
 
   def shop_advertisement_html
-    shop_id = Discourse.cache.read(:store_ids_for_advertisement)&.sample
     current_user = object&.guardian&.user&.id
 
-    best_answer_id = object.topic.custom_fields["accepted_answer_post_id"]&.to_i
+    best_answer_id = object.topic.custom_fields["accepted_answer_post_id"]
 
-    if best_answer_id.nil?
-      # case for topic owner
-      target_user_id = user_id
-    else
-      # case for best answer
-      post = object.posts.select { |post| post[:id] == best_answer_id }
+    target_user_id = best_answer_id ? Post.find(best_answer_id).user_id : object.user_id
 
-      target_user_id = post.first[:user_id].to_i
-    end
-
-    custom_field = UserCustomField.select(:value).where(user_id: target_user_id, name: 'storeId').last
-
-    shop_id = custom_field unless custom_field.nil?
+    shop_id = UserCustomField.where(user_id: target_user_id, name: 'storeId').first&.value || 
+              Discourse.cache.read(:store_ids_for_advertisement)&.sample
 
     DesignBundles.product_display_html(shop_id, current_user)
   end
