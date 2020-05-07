8c1acbca02dca238bf2b00bafcad1d63774cf1c4
Gotoinc <root@gotoinc.co>
2020-05-07 14:25:20 +0300

    Add store choosing

--- app/serializers/shop_advertisement_serializer_mixin.rb
+++ app/serializers/shop_advertisement_serializer_mixin.rb
@@ -7,7 +7,24 @@ module ShopAdvertisementSerializerMixin
 
   def shop_advertisement_html
     shop_id = Discourse.cache.read(:store_ids_for_advertisement)&.sample
-    user_id = object&.guardian&.user&.id
-    DesignBundles.product_display_html(shop_id, user_id)
+    current_user = object&.guardian&.user&.id
+
+    best_answer_id = object.topic.custom_fields["accepted_answer_post_id"]&.to_i
+
+    if best_answer_id.nil?
+      # case for topic owner
+      target_user_id = user_id
+    else
+      # case for best answer
+      post = object.posts.select { |post| post[:id] == best_answer_id }
+
+      target_user_id = post.first[:user_id].to_i
+    end
+
+    custom_field = UserCustomField.select(:value).where(user_id: target_user_id, name: 'storeId').last
+
+    shop_id = "here must be storeId" unless custom_field.nil?
+
+    DesignBundles.product_display_html(shop_id, current_user)
   end
 end
