aa2cb8fa1210a48628bdaf67b24ffb068e8ac70f
Gotoinc <root@gotoinc.co>
2020-05-07 16:35:44 +0300

    Bump version to 0.6

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 
 # name: DiscourseDesignBundles
 # about: Discourse plugin to implement all extensions needed for Design Bundles company
-# version: 0.5
+# version: 0.6
 # authors: Gotoinc
 # url: https://github.com/gotoinc/discourse-design-bundles/
 
